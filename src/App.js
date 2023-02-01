bc60b8d2da0e9476ba80cc085a2231c689770007
Prakhar <Prakhars418@gmail.com>
2023-02-01 20:06:03 +0530

    e commerce site completed

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Header from "./Header"; //Include Header
+import Footer from "./Footer"; //Include Footer
+import Home from "./Home";
+import Shop from "./Shop";
+import Cart from "./Cart";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
